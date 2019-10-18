65aa99d553ff28b1b4263554ac5047927930f6eb
Federico Iglesias <fede.iglesiasc@gmail.com>
2019-10-18 13:48:22 +0200

    Asume yes param

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 # CentOS install in AWS
 
 # Update yum packages
-  sudo yum update
-  sudo yum upgrade
+  sudo yum -y update
+  sudo yum -y upgrade
 
 # Install wget and git
-  sudo yum install -y git
+  sudo yum -y install git
 
 # Install ZSH from source
   sudo yum -y install gcc
   sudo yum -y install ncurses-devel
-  sudo yum install -y wget
+  sudo yum -y install wget
   
   wget http://www.zsh.org/pub/zsh-5.7.1.tar.xz -P ~/ && tar xf ~/zsh-5.7.1.tar.xz && cd ~/zsh-5.7.1
   ./configure && make && sudo make install
