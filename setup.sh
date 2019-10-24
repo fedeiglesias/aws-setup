ea459da93037e478675b0c665f6bb0eb7590fe7e
Federico Iglesias <fede.iglesiasc@gmail.com>
2019-10-24 17:03:41 +0200

    Main project setup

--- setup.sh
+++ setup.sh
@@ -3,9 +3,25 @@
 # Show menu prompt
 echo '####################################################################'
 
-# Git & Git Hooks
-read -e -p "INSTALL GIT [n/y]: " -i "y" install_git
-read -e -p "INSTALL GOLANG (git hooks) [n/y]: " -i "y" install_golang
+read -e -p "MAIN PROJECT NAME: " -i "home" main_project_name
+read -e -p "IS MAIN PROJECT LINKED TO GIT REPO ? " -i "y" main_project_linked_to_git
+
+if [ $main_project_linked_to_git == "y" ] then
+  install_git="y"
+  read -e -p "MAIN PROJECT GIT (http for git clone): " -i "" main_project_git_repo
+fi
+
+# If user dont want to link main project to a git repo
+# ask him anyway if want to install git alone
+if [ $main_project_linked_to_git == "n" ] then
+  read -e -p "INSTALL GIT [n/y]: " -i "y" install_git
+fi
+
+# Golang & Webhook
+read -e -p "INSTALL GOLANG (for git hooks) [n/y]: " -i "y" install_golang
+if [ $install_golang == "y" ] then
+  read -e -p "INSTALL WEBHOOK (for git hooks) [n/y]: " -i "y" install_webhook
+fi
 
 read -e -p "INSTALL NGINX [n/y]: " -i "y" install_nginx
 read -e -p "INSTALL ACME.SH [n/y]: " -i "y" install_acmesh
@@ -21,6 +37,12 @@ fi
 
 echo '####################################################################'
 
+# create folder for projects
+mkdir ~/projects
+
+# create folder for main project
+mkdir ~/projects/$main_project_name
+
 # Update yum packages
 sudo yum -y update
 sudo yum -y upgrade
@@ -29,19 +51,57 @@ sudo yum -y upgrade
 if [ $install_git == "y" ] 
 then
   sudo yum -y install git
+
+  #link main project to git repo
+  if [ $main_project_linked_to_git == "y" ] 
+  then
+    # move to project dir
+    cd ~/projects/$main_project_name
+    # init git repo & add remote origin
+    git init && git remote add origin $main_project_git_repo
+    #move to home
+    cd ~/
+  fi
 fi
+
+
+
+# git init
+# git remote add origin git@github.com:fedeiglesiasc/fedeiglesias.com.git
 
 #install Golang
 if [ $install_golang == "y" ] 
 then
   # Get LTS version
   GOLANG_VERSION="`wget -qO- https://golang.org/VERSION?m=text`"
+  # Move to tmp
   cd /tmp
+  # Get installer
   wget https://dl.google.com/go/$GOLANG_VERSION.linux-amd64.tar.gz
+  # decompress
   sudo tar -C /usr/local -xzf $GOLANG_VERSION.linux-amd64.tar.gz
+  # install
   export PATH=$PATH:/usr/local/go/bin
+  # remove tmp files
   rm -rf $GOLANG_VERSION.linux-amd64.tar.gz
+  # move to home
+  cd ~/
+
+  #install Golang
+  if [ $install_webhook == "y" ] 
+  then
+    # install webhook
+    go get github.com/adnanh/webhook
+    # create dir for webhooks
+    mkdir ~/webhooks
+    touch ~/webhooks/hooks.json
+  fi
 fi
+
+
+
+
+
 
 # If nginx installed restart it
 if [ $install_nginx == "y" ] 
